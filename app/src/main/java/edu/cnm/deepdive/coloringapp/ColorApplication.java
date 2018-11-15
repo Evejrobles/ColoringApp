29ce6ebba4ef82487b2dcd18eda394e593d128ae
Eve <evejrobles@icloud.com>
2018-11-15 16:36:45 -0700

    added google sign in

--- app/src/main/java/edu/cnm/deepdive/coloringapp/ColorApplication.java
+++ app/src/main/java/edu/cnm/deepdive/coloringapp/ColorApplication.java
@@ -2,6 +2,10 @@ package edu.cnm.deepdive.coloringapp;
 
 import android.app.Application;
 import com.facebook.stetho.Stetho;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import edu.cnm.deepdive.coloringapp.model.db.ColoringAppDatabase;
 
 
@@ -11,17 +15,48 @@ import edu.cnm.deepdive.coloringapp.model.db.ColoringAppDatabase;
 public class ColorApplication extends Application {
 
   private ColoringAppDatabase database;
+  private GoogleSignInClient client;
+  private GoogleSignInAccount account;
+  private static ColorApplication instance;
+
+
+  public static ColorApplication getInstance() {
+    return instance;
+  }
 
   @Override
   public void onCreate() {
     super.onCreate();
+    instance = this;
     Stetho.initializeWithDefaults(this);
     ColoringAppDatabase.getInstance(this);
+    GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestEmail()
+        .requestId()
+        //Include requestIdToken if we're using Google Sign-in for authenticating on back end.
+        .build();
+    client = GoogleSignIn.getClient(this, options);
   }
 
   @Override
   public void onTerminate() {
     ColoringAppDatabase.forgetInstance();
     super.onTerminate();
+  }
+
+  public GoogleSignInClient getClient() {
+    return client;
+  }
+
+  public void setClient(GoogleSignInClient client) {
+    this.client = client;
+  }
+
+  public GoogleSignInAccount getAccount() {
+    return account;
+  }
+
+  public void setAccount(GoogleSignInAccount account) {
+    this.account = account;
   }
 }
